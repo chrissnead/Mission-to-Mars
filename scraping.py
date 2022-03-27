8c181e3039fdb0334d7f6a6edda010b1fa37f7ae
Christopher Snead <christophersnead@Christophers-MacBook-Pro.local>
2022-03-27 16:37:08 -0700

    added styling to index.html

--- scraping.py
+++ scraping.py
@@ -19,7 +19,8 @@ def scrape_all():
         "news_paragraph": news_paragraph,
         "featured_image": featured_image(browser),
         "facts": mars_facts(),
-        "last_modified": dt.datetime.now()
+        "last_modified": dt.datetime.now(),
+        "hemispheres": hemispheres(browser)
     }
 
     # Stop webdriver and return data
@@ -96,6 +97,32 @@ def mars_facts():
 
     # Convert dataframe into HTML format, add bootstrap
     return df.to_html(classes="table table-striped")
+
+def hemispheres(browser):
+    # Use browser to visit the URL 
+    url = 'https://marshemispheres.com/'
+
+    browser.visit(url)
+
+    hemisphere_image_urls = []
+
+    # Retrieve the image urls and titles for each hemisphere
+    for i in range(4):
+        hemispheres = {}
+        
+        browser.find_by_css('a.product-item h3')[i].click()
+
+        element = browser.links.find_by_text('Sample').first
+        img_url = element['href']
+        title = browser.find_by_css('h2.title').text
+        
+        hemispheres['img_url'] = img_url
+        hemispheres['title'] = title
+        hemisphere_image_urls.append(hemispheres)
+        
+        browser.back()
+
+        return hemisphere_image_urls
 
 if __name__ == "__main__":
 
